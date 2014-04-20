c2ee7c3f17a88c077b0cb553191b80a7ec2838df
Minh-Quan Tran <account@itscaro.me>
2014-04-20 14:22:21 +0200

    flickr

--- library/Itscaro/Service/Flickr/Model/ModelAbstract.php
+++ library/Itscaro/Service/Flickr/Model/ModelAbstract.php
@@ -12,9 +12,6 @@ abstract class ModelAbstract {
     public function __construct(array $rawData = array())
     {
         foreach ($rawData as $_key => $_data) {
-            if (!property_exists($this, $_key)) {
-                throw new \Exception(sprintf("Class '%s' does not have property '%s'.", get_class($this), $_key));
-            }
             $this->{$_key} = $_data;
         }
     }
